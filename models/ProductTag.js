42d85c343c7c834d30369e4e7744df3f4655db90
Nicole Gile <nicolegile94@gmail.com>
2020-11-10 23:11:16 -0600

    added columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      //reference the product model's id
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //reference the tag model's id
+    }
   },
   {
     sequelize,
